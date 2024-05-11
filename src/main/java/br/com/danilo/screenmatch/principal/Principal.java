6b4681fd3867989fcd249908459b9897dbb70392
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2024-05-10 23:57:56 -0300

    criacao de novos modelos e uso de streams

--- src/main/java/br/com/danilo/screenmatch/principal/Principal.java
+++ src/main/java/br/com/danilo/screenmatch/principal/Principal.java
@@ -3,12 +3,15 @@ package br.com.danilo.screenmatch.principal;
 import br.com.danilo.screenmatch.model.DadosEpisodio;
 import br.com.danilo.screenmatch.model.DadosSerie;
 import br.com.danilo.screenmatch.model.DadosTemporada;
+import br.com.danilo.screenmatch.model.Episodio;
 import br.com.danilo.screenmatch.service.ConsumoApi;
 import br.com.danilo.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -35,15 +38,27 @@ public class Principal {
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
-		temporadas.forEach(System.out::println);
-//
-//        for (int i = 0; i < dados.totalTemporadas(); i++) {
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for (int j = 0; j < episodiosTemporada.size(); j++) {
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
 
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+		temporadas.forEach(System.out::println);
+
+//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 Episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream().map(d -> new Episodio(t.numero(),d)))
+                .collect(Collectors.toList());
+
+        System.out.println("\nEpisódios por Temporada: ");
+        episodios.forEach(System.out::println);
     }
 }
